a8e734a37fe6d8ac5d2349d9698c78e9925536ef
thanhtrang16490 <thanhtrang16490@gmail.com>
2025-08-03 13:12:57 +0700

    Add mobile help page and guide components with animations

--- src/components/mobile/BottomNavWrapper.tsx
+++ src/components/mobile/BottomNavWrapper.tsx
@@ -35,14 +35,14 @@ const BottomNavWrapper: React.FC = () => {
       text: 'Video'
     },
     {
-      href: '/feed',
+      href: '/help',
       icon: (
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 20H5a2 2 0 01-2-2V6a2 2 0 012-2h10a2 2 0 012 2v1m2 13a2 2 0 01-2-2V7m2 13a2 2 0 002-2V9a2 2 0 00-2-2h-2m-4-3H9M7 16h6M7 8h6v4H7V8z" />
+          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M8.228 9c.549-1.165 2.03-2 3.772-2 2.21 0 4 1.343 4 3 0 1.4-1.278 2.575-3.006 2.907-.542.104-.994.54-.994 1.093m0 3h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
         </svg>
       ),
       isImage: false,
-      text: 'Feed'
+      text: 'Trợ giúp'
     },
     {
       href: '/categories',
